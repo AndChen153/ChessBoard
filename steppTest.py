cb796a2a6bcf18b8333e60b27dcc9f4ef59cd855
AndChen153 <achen1192@gmail.com>
2019-12-30 11:48:25 -0600

    dsf

--- steppTest.py
+++ steppTest.py
@@ -63,8 +63,9 @@ while (True):
         st2 = threading.Thread(target=stepper_worker, args=(myStepper2, 300, dir, stepstyles[2]))
         st2.start()'''
 
-myStepper1.step(10, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
-myStepper1.step(10, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
+myStepper1.step(30, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
+myStepper1.step(30, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+print("set")
 
 while (True):
     myStepper1.step(600, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
