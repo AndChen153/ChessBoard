b9e727b7b72c9a35e988557e7c44d5e4756d507a
AndChen153 <achen1192@gmail.com>
2019-12-29 19:08:07 -0600

    changed speed

--- steppTest.py
+++ steppTest.py
@@ -16,12 +16,12 @@ def turnOffMotors():
 atexit.register(turnOffMotors)
 
 myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
-myStepper.setSpeed(30)             #  RPM (proportional to seconds per step)
+myStepper.setSpeed(60)             #  RPM (proportional to seconds per step)
 
 myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
 
 myStepper = mh.getStepper(200, 2)  # 200 steps/rev, motor port #1
-myStepper.setSpeed(30)             #  RPM (proportional to seconds per step)
+myStepper.setSpeed(60)             #  RPM (proportional to seconds per step)
 
 myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
 
