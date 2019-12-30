84f134c4ee202cd6dc67196c4a6b5fd3a4f5c3fc
AndChen153 <achen1192@gmail.com>
2019-12-30 11:33:04 -0600

    dif loop

--- steppTest.py
+++ steppTest.py
@@ -18,6 +18,7 @@ def turnOffMotors():
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    
 atexit.register(turnOffMotors)
 
 stepstyles = [Adafruit_MotorHAT.SINGLE, Adafruit_MotorHAT.DOUBLE, Adafruit_MotorHAT.INTERLEAVE, Adafruit_MotorHAT.MICROSTEP]
@@ -32,27 +33,33 @@ myStepper1 = mh.getStepper(200, 1)      # 200 steps/rev, motor port #1
 myStepper2 = mh.getStepper(200, 2)      # 200 steps/rev, motor port #1
 myStepper1.setSpeed(45)          # 30 RPM
 myStepper2.setSpeed(45)          # 30 RPM
+'''
+st1 = threading.Thread(target=stepper_worker, args=(myStepper1, 300, Adafruit_MotorHAT.FORWARD, stepstyles[2]))
+st1.start()
+st2 = threading.Thread(target=stepper_worker, args=(myStepper2, 300, Adafruit_MotorHAT.FORWARD, stepstyles[2]))
+st2.start()'''
 
-a=0
+direction1=True
+direction2=True
 
 while (True):
-    if (a==0):
-        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, 300, Adafruit_MotorHAT.FORWARD, stepstyles[2]))
+    if not st1.isAlive():
+        if direction1:
+            dir=Adafruit_MotorHAT.FORWARD
+            direction1=False
+        else:
+            dir=Adafruit_MotorHAT.BACKWARD
+            direction1=True
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, 300, dir, stepstyles[2]))
         st1.start()
 
-        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, 300, Adafruit_MotorHAT.FORWARD, stepstyles[2]))
+    if not st2.isAlive():
+        if direction2:
+            dir=Adafruit_MotorHAT.FORWARD
+            direction2=False
+        else:
+            dir=Adafruit_MotorHAT.BACKWARD
+            direction2=True
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, 300, dir, stepstyles[2]))
         st2.start()
-        a=a+1
-
-    elif (a==1):
-
-        if not st1.isAlive():
-            st1 = threading.Thread(target=stepper_worker, args=(myStepper1, 300, Adafruit_MotorHAT.BACKWARD, stepstyles[2]))
-            st1.start()
-
-
-        if not st2.isAlive():
-            st2 = threading.Thread(target=stepper_worker, args=(myStepper2, 300, Adafruit_MotorHAT.BACKWARD, stepstyles[2]))
-            st2.start()
-            a=0
         
