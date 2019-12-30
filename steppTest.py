70cd39ca3d38a685efdf766fed38faba88293063
AndChen153 <achen1192@gmail.com>
2019-12-29 19:23:33 -0600

    DOUBLE

--- steppTest.py
+++ steppTest.py
@@ -12,8 +12,8 @@ def turnOffMotors():
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-
 atexit.register(turnOffMotors)
+
 
 myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
 myStepper.setSpeed(30)             #  RPM (proportional to seconds per step)
@@ -21,23 +21,23 @@ myStepper.setSpeed(30)             #  RPM (proportional to seconds per step)
 myStepper2 = mh.getStepper(200, 2)  # 200 steps/rev, motor port #1
 myStepper2.setSpeed(30)             #  RPM (proportional to seconds per step)
 
-myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
+myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.DOUBLE)
 
 time.sleep(0.5)
 
-myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
+myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.DOUBLE)
 
 time.sleep(0.5)
 
-myStepper.step(100, Adafruit_MotorHAT.BACKWARD,  Adafruit_MotorHAT.INTERLEAVE)
+myStepper.step(100, Adafruit_MotorHAT.BACKWARD,  Adafruit_MotorHAT.DOUBLE)
 
 time.sleep(0.5)
 
-myStepper2.step(100, Adafruit_MotorHAT.BACKWARD,  Adafruit_MotorHAT.INTERLEAVE)
+myStepper2.step(100, Adafruit_MotorHAT.BACKWARD,  Adafruit_MotorHAT.DOUBLE)
 
 time.sleep(0.5)
 
-myStepper2.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
+myStepper2.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.DOUBLE)
 '''while (True):
     print("Single coil steps")
     myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
