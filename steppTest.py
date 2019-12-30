6c204bbac0d1653b37587e98fd9c89e99ff16353
AndChen153 <achen1192@gmail.com>
2019-12-29 19:05:28 -0600

    changed motor type

--- steppTest.py
+++ steppTest.py
@@ -18,12 +18,12 @@ atexit.register(turnOffMotors)
 myStepper = mh.getStepper(200, 1)  # 200 steps/rev, motor port #1
 myStepper.setSpeed(30)             #  RPM (proportional to seconds per step)
 
-myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.DOUBLE)
+myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
 
 myStepper = mh.getStepper(200, 2)  # 200 steps/rev, motor port #1
 myStepper.setSpeed(30)             #  RPM (proportional to seconds per step)
 
-myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.DOUBLE)
+myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.INTERLEAVE)
 
 
 '''while (True):
