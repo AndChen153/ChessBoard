61c84168988b11d28f53ff2fa2ba0218aeec1b5f
Andrew Chen <achen1192@gmail.com>
2020-05-27 20:07:44 -0500

    finished chessmove first draft

--- ChessMove.py
+++ ChessMove.py
@@ -5,13 +5,14 @@ import time
 import atexit
 import threading
 import random
+import sys
 
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT()
 
 # create empty threads (these will hold the stepper 1 and 2 threads) so both motors can be ran at the same time
-st1 = threading.Thread()
-st2 = threading.Thread()
+st1 = threading.Thread()    #x axis
+st2 = threading.Thread()    #y axis
 
 
 XAxisStepper = mh.getStepper(200, 1)      # 200 steps/rev (1.8 degrees per step), motor port #1
@@ -26,7 +27,7 @@ stepDirection = [Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.BACKWARD]
 #                   0                               1
 
 #number of steps per spaces on the chessboard
-
+steps=150
 
 # turns off motors at exit of program
 def turnOffMotors():
@@ -56,24 +57,70 @@ def jiggle(stepper):
         if not st2.isAlive():
             st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, 5, stepDirection[0], stepStyles[2],))
             st2.start()
-        if not st1.isAlive():
+        if not st2.isAlive():
             st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, 5, stepDirection[1], stepStyles[2],))
             st2.start()
 
+#direction -> 0 is forward 1 is backward
 def translation(xPlaces, xDirection, yPlaces, yDirection):
-    xPlaces=xPlaces*
+    xPlaces = int(xPlaces)*steps
+    yPlaces = int(yPlaces)*steps
+    dirx = stepDirection[int(xDirection)]
+    diry = stepDirection[int(yDirection)]
+
+    #moving in a diagonal
     if xPlaces == yPlaces:
         if not st1.isAlive():
-            st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, int(), dirx, stepStyles[2],))
+            st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, xPlaces, dirx, stepStyles[2],))
             st1.start()
         if not st2.isAlive():
-            st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, int(y), diry, stepStyles[2],))
-            st2.start()        
+            st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, yPlaces, diry, stepStyles[2],))
+            st2.start()
 
+    #un-diagonal movement
+    elif xPlaces > yPlaces:
+        xTemp = xPlaces - yPlaces
+        #diagonal
+        if not st1.isAlive():
+            st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, yPlaces, dirx, stepStyles[2],))
+            st1.start()
+        if not st2.isAlive():
+            st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, yPlaces, diry, stepStyles[2],))
+            st2.start()
+        #straight
+        if not st1.isAlive():
+            st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, xTemp, dirx, stepStyles[2],))
+            st1.start()
+        if st1.isAlive():
+            jiggle("y")
+    
+    elif yPlaces > xPlaces:
+        yTemp = yPlaces-xPlaces
+        #diagonal
+        if not st1.isAlive():
+            st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, xPlaces, dirx, stepStyles[2],))
+            st1.start()
+        if not st2.isAlive():
+            st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper,xPlaces, diry, stepStyles[2],))
+            st2.start()
+        #straight
+        if not st2.isAlive():
+            st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, yTemp, diry, stepStyles[2],))
+            st2.start()
+        if st1.isAlive():
+            jiggle("x")
 
+    turnOffMotors()
+
+#sys.argv=(xPlaces, xDirection, yPlaces, yDirection)
+#            0          1           2          3
+translation(sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[3])
+
+'''
 if not st1.isAlive():
     st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, int(x), dirx, stepStyles[2],))
     st1.start()
 if not st2.isAlive():
     st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, int(y), diry, stepStyles[2],))
     st2.start()
+'''
