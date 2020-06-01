5b943a7c6342ab4bb5d8cc3e2a376c992484ad92
AndChen153 <achen1192@gmail.com>
2020-06-01 12:30:12 -0500

    sld

--- ChessMove.py
+++ ChessMove.py
@@ -152,10 +152,8 @@ if len(sys.argv)>3:
     translation(sys.argv[0],sys.argv[1],sys.argv[2],sys.argv[3])
 
 
-
-for i in range (3):
-    translation(3,1,1,1)
-    time.sleep(0.1)
+translation(3,1,1,1)
+time.sleep(0.1)
 
 
 
