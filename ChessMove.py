83e49b8fad04d70d4b1bd22528e2af5193fc9275
Andrew Chen <achen1192@gmail.com>
2020-06-09 15:06:03 -0500

    electromagnet integrated

--- ChessMove.py
+++ ChessMove.py
@@ -1,6 +1,7 @@
 
 
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+import RPi.GPIO as GPIO
 import time
 import atexit
 import threading
@@ -14,15 +15,22 @@ mh = Adafruit_MotorHAT()
 st1 = threading.Thread()
 st2 = threading.Thread()
 
+# setup gpio pin for relay
+channel = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.OUT)
+
+
 # turns off motors at exit of program
 def turnOffMotors():
     mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    GPIO.cleanup()
 atexit.register(turnOffMotors)
 
-#runs motors
+# runs motors
 def stepper_worker(stepper, numsteps, direction, style):
     print("Steppin!")
     stepper.step(numsteps, direction, style)
@@ -33,16 +41,16 @@ YAxisStepper = mh.getStepper(200, 2)      # 200 steps/rev (1.8 degrees per step)
 XAxisStepper.setSpeed(5000)
 YAxisStepper.setSpeed(5000)
 
-#use double or interleave(half the distnace double moves)
+# use double or interleave(half the distnace double moves)
 stepStyles = [Adafruit_MotorHAT.SINGLE, Adafruit_MotorHAT.DOUBLE, Adafruit_MotorHAT.INTERLEAVE, Adafruit_MotorHAT.MICROSTEP]
 #                   0                               1                           2                           3
 stepDirection = [Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.BACKWARD]
 #                   0                               1
 stepDirectiony = [Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.FORWARD]
-#so motors move same direction    0                               1
+# so motors move same direction    0                               1
 
 
-#number of steps per spaces on the chessboard
+# number of steps per spaces on the chessboard
 steps=215
 ysteps=220
 incrementer=0
@@ -117,7 +125,7 @@ def jiggleY(yTemp):
 
 
 
-#direction -> 0 is forward 1 is backward
+# direction -> 0 is forward 1 is backward
 def translation(xPlaces, xDirection, yPlaces, yDirection):
     global st1
     global st2
@@ -128,7 +136,7 @@ def translation(xPlaces, xDirection, yPlaces, yDirection):
     diry = stepDirectiony[int(yDirection)]
 
 
-    #moving in a diagonal
+    # moving in a diagonal
     if xPlaces == yPlaces:
         if not st1.is_alive():
             st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, xPlaces, dirx, stepStyles[1],))
@@ -139,12 +147,12 @@ def translation(xPlaces, xDirection, yPlaces, yDirection):
 
 
 
-    #un-diagonal movement
+    # un-diagonal movement
     elif xPlaces > yPlaces:
 
         xTemp = xPlaces - yPlaces
 
-        #diagonal
+        # diagonal
         if not st1.is_alive():
             st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, int(yPlaces), dirx, stepStyles[1],))
             st1.start()
@@ -152,21 +160,21 @@ def translation(xPlaces, xDirection, yPlaces, yDirection):
             st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, int(yPlaces), diry, stepStyles[1],))
             st2.start()
 
-        #straight
+        # straight
         while st1.is_alive() and st2.is_alive():
             print("waiting.. move x ")
             time.sleep(0.5)
         if not st1.is_alive():
             st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, xTemp, dirx, stepStyles[1],))
             st1.start()
-        #uses other motor for a small amount to get rid of st1 not completing full amount of steps bc of weird motor hat
+        # uses other motor for a small amount to get rid of st1 not completing full amount of steps bc of weird motor hat
         jiggleX(xTemp)
 
     elif yPlaces > xPlaces:
 
         yTemp = yPlaces-xPlaces
         print(yTemp)
-        #diagonal
+        # diagonal
         if not st1.is_alive():
             st1 = threading.Thread(target=stepper_worker, args=(XAxisStepper, int(xPlaces), dirx, stepStyles[1],))
             st1.start()
@@ -174,21 +182,28 @@ def translation(xPlaces, xDirection, yPlaces, yDirection):
             st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper,int(xPlaces), diry, stepStyles[1],))
             st2.start()
 
-        #straight
+        # straight
         while st1.is_alive() and st2.is_alive():
             print("waiting.. move y ")
             time.sleep(0.5)
         if not st2.is_alive():
             st2 = threading.Thread(target=stepper_worker, args=(YAxisStepper, yTemp, diry, stepStyles[1],))
             st2.start()
-        #uses other motor for a small amount to get rid of st2 not completing full amount of steps bc of weird motor hat
+        # uses other motor for a small amount to get rid of st2 not completing full amount of steps bc of weird motor hat
         jiggleY(yTemp)
     
     
     turnOffMotors()
 
-#sys.argv=(xPlaces, xDirection, yPlaces, yDirection)
+# sys.argv=(xPlaces, xDirection, yPlaces, yDirection)
 #            0          1           2          3
+
+magnet=input("1(high) or 0(low)")
+if magnet == "1":
+    GPIO.output(channel, GPIO.HIGH)
+elif magnet == "0":
+    GPIO.output(channel, GPIO.LOW)
+
 a=input("places")
 b=input("direction")
 c=input("places")
