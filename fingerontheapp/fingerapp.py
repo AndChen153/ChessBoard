8c37de5cbd99f49dba485e6976212d06308fdd36
Andrew Chen <achen1192@gmail.com>
2020-06-29 23:31:19 -0500

    fix

--- fingerontheapp/fingerapp.py
+++ fingerontheapp/fingerapp.py
@@ -166,8 +166,8 @@ while True:
 
     xNum = int(xPercent*xSteps)
     yNum = int(yPercent*ySteps)
-    xNum -= 90
-    yNum -= 90
+    xNum -= 45
+    yNum -= 45
 
     #translation(xNum, xdir, yNum, ydir)
     translation(xNum, yNum)
