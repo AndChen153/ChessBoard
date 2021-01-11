3fa669b797e8a9ab8818558d873269eafc07db02
andrew <achen1192@gmail.com>
2021-01-11 16:38:31 -0600

    v2

--- ChessMoveV2.py
+++ ChessMoveV2.py
@@ -4,6 +4,10 @@ import atexit
 import threading
 import random
 
+STEPSTYLE = Adafruit_MotorHAT.DOUBLE
+FORWARD = Adafruit_MotorHAT.FORWARD
+BACKWARD = Adafruit_MotorHAT.BACKWARD
+
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT()
 
@@ -33,33 +37,22 @@ def stepper_worker(stepper, numsteps, direction, style):
 
 while (True):
     if not st1.isAlive():
-        randomdir = random.randint(0, 1)
-        print("Stepper 1"),
-        if (randomdir == 0):
-            dir = Adafruit_MotorHAT.FORWARD
-            print("forward"),
-        else:
-            dir = Adafruit_MotorHAT.BACKWARD
-            print("backward"),
-        randomsteps = random.randint(100,300)
-        print("%d steps" % randomsteps)
-        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, randomsteps, dir, stepstyles[3],))
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, 200, FORWARD, STEPSTYLE,))
         st1.start()
 
     if not st2.isAlive():
-        print("Stepper 2"),
-        randomdir = random.randint(0, 1)
-        if (randomdir == 0):
-            dir = Adafruit_MotorHAT.FORWARD
-            print("forward"),
-        else:
-            dir = Adafruit_MotorHAT.BACKWARD
-            print("backward"),
-
-        randomsteps = random.randint(100,300)
-        print("%d steps" % randomsteps)
-
-        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, randomsteps, dir, stepstyles[3],))
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, 200, FORWARD, STEPSTYLE,))
         st2.start()
     
     time.sleep(0.1)
+
+    if not st1.isAlive():
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, 200, BACKWARD, STEPSTYLE,))
+        st1.start()
+
+    if not st2.isAlive():
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, 200, BACKWARD, STEPSTYLE,))
+        st2.start()
+    
+    time.sleep(0.1)
+    
