4c60d08d2ea1cf90a258ba28e8800cbe2ee231b4
andrew <achen1192@gmail.com>
2021-01-11 18:35:50 -0600

    v2

--- ChessMoveV2.py
+++ ChessMoveV2.py
@@ -4,7 +4,9 @@ import atexit
 import threading
 import random
 
-stepstyles = [Adafruit_MotorHAT.SINGLE, Adafruit_MotorHAT.DOUBLE, Adafruit_MotorHAT.INTERLEAVE, Adafruit_MotorHAT.MICROSTEP]
+STEPSTYLE = Adafruit_MotorHAT.DOUBLE
+FORWARD = Adafruit_MotorHAT.FORWARD
+BACKWARD = Adafruit_MotorHAT.BACKWARD
 
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT()
@@ -13,46 +15,68 @@ mh = Adafruit_MotorHAT()
 st1 = threading.Thread()
 st2 = threading.Thread()
 
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
 myStepper1 = mh.getStepper(200, 1)      # 200 steps/rev, motor port #1
 myStepper2 = mh.getStepper(200, 2)      # 200 steps/rev, motor port #1
 myStepper1.setSpeed(60)          # 30 RPM
 myStepper2.setSpeed(60)          # 30 RPM
 
-class MotorMove(object):
-    def __init(self):
-        STEPSTYLE = Adafruit_MotorHAT.DOUBLE
-        FORWARD = Adafruit_MotorHAT.FORWARD
-        BACKWARD = Adafruit_MotorHAT.BACKWARD
+stepstyles = [Adafruit_MotorHAT.SINGLE, Adafruit_MotorHAT.DOUBLE, Adafruit_MotorHAT.INTERLEAVE, Adafruit_MotorHAT.MICROSTEP]
 
-        global st1
-        global st2
-        global myStepper1
-        global myStepper2
+def stepper_worker(stepper, numsteps, direction, style):
+    #print("Steppin!")
+    stepper.step(numsteps, direction, style)
+    #print("Done")
+def stepper_outandback(distance):
+    global st1
+    global st2
+    if not st1.isAlive():
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, distance, FORWARD, STEPSTYLE,))
+        st1.start()
+
+    if not st2.isAlive():
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, distance, FORWARD, STEPSTYLE,))
+        st2.start()
+
+    st1.join()
+    st2.join()
+
+    if not st1.isAlive():
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, distance, BACKWARD, STEPSTYLE,))
+        st1.start()
+
+    if not st2.isAlive():
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, distance, BACKWARD, STEPSTYLE,))
+        st2.start()
     
-    def stepper_worker(self, stepper, numsteps, direction, style):
-        #print("Steppin!")
-        stepper.step(numsteps, direction, style)
-        #print("Done")
+def stepper_outandside(xdistance, ydistance):
+    global st1
+    global st2
+    if xdistance < ydistance:
+        distance = xdistance
+        remain 
+    if not st1.isAlive():
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, distance, FORWARD, STEPSTYLE,))
+        st1.start()
 
-    def stepper_outandback(self, distance):
-        if not st1.isAlive():
-            st1 = threading.Thread(target=stepper_worker, args=(myStepper1, distance, FORWARD, STEPSTYLE,))
-            st1.start()
+    if not st2.isAlive():
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, distance, FORWARD, STEPSTYLE,))
+        st2.start()
 
-        if not st2.isAlive():
-            st2 = threading.Thread(target=stepper_worker, args=(myStepper2, distance, FORWARD, STEPSTYLE,))
-            st2.start()
+    st1.join()
+    st2.join()
 
-        st1.join()
-        st2.join()
+    if not st1.isAlive():
+        st1 = threading.Thread(target=stepper_worker, args=(myStepper1, distance, BACKWARD, STEPSTYLE,))
+        st1.start()
 
-        if not st1.isAlive():
-            st1 = threading.Thread(target=stepper_worker, args=(myStepper1, distance, BACKWARD, STEPSTYLE,))
-            st1.start()
-
-        if not st2.isAlive():
-            st2 = threading.Thread(target=stepper_worker, args=(myStepper2, distance, BACKWARD, STEPSTYLE,))
-            st2.start()
-
-Chess = MotorMove()
-Chess.stepper_outandback(450)
+    if not st2.isAlive():
+        st2 = threading.Thread(target=stepper_worker, args=(myStepper2, distance, BACKWARD, STEPSTYLE,))
+        st2.start()
