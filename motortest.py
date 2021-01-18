ea4f1081c00d21f4649eeb9ba26a8428f846a34f
andrew <achen1192@gmail.com>
2021-01-18 14:06:21 -0600

    new

--- motortest.py
+++ motortest.py
@@ -10,12 +10,21 @@ SPR = 200  # Steps per Rotation (360/1.8)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CCW)
+GPIO.output(DIR, CW)
 
 step_count = SPR
 delay = 0.005
 
-for x in range(1000):
+for x in range(200):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(1)
+GPIO.output(DIR, CCW)
+
+for x in range(200):
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
