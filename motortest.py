cabbc2204a4527711b446a717c76a80bb1db7494
andrew <achen1192@gmail.com>
2021-01-18 14:54:28 -0600

    new

--- motortest.py
+++ motortest.py
@@ -1,8 +1,10 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-DIR = 21 # Directional GPIO Pin
-STEP = 20 # Step GPIO Pin
+DIR1 = 21 # Directional GPIO Pin
+STEP1 = 20 # Step GPIO Pin
+DIR2 = 16 # Directional GPIO Pin
+STEP2 = 12 # Step GPIO Pin
 CW = GPIO.HIGH # CLockwise Rotation
 CCW = GPIO.LOW # Counter Clockwise Rotation
 LOW = GPIO.LOW
@@ -10,12 +12,18 @@ HIGH = GPIO.HIGH
 SPR = 200  # Steps per Rotation (360/1.8)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
+
+GPIO.setup(DIR1, GPIO.OUT)
+GPIO.setup(STEP1, GPIO.OUT)
+GPIO.setup(DIR2, GPIO.OUT)
+GPIO.setup(STEP2, GPIO.OUT)
+
+GPIO.output(DIR1, CW)
+GPIO.output(DIR2, CW)
 
 MODE = (14, 15, 18)
 GPIO.setup(MODE, GPIO.OUT)
+
 
 RESOLUTION = {'Full': (LOW, LOW, LOW),
               'Half': (HIGH, LOW, LOW),
@@ -30,18 +38,23 @@ step_count = 200 * 32
 delay = 0.0025 / 32
 
 for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
+    GPIO.output(STEP1, GPIO.HIGH)
+    GPIO.output(STEP2, GPIO.HIGH)
     sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
+    GPIO.output(STEP1, GPIO.LOW)
+    GPIO.output(STEP2, GPIO.LOW)
     sleep(delay)
 
 sleep(1)
-GPIO.output(DIR, CCW)
+GPIO.output(DIR1, CCW)
+GPIO.output(DIR2, CCW)
 
 for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
+    GPIO.output(STEP1, GPIO.HIGH)
+    GPIO.output(STEP2, GPIO.HIGH)
     sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
+    GPIO.output(STEP2, GPIO.LOW)
     sleep(delay)
 
 GPIO.cleanup()
