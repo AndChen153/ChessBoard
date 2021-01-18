e1c30add9581820366ced1caaaabacd263cd4188
andrew <achen1192@gmail.com>
2021-01-18 16:57:52 -0600

    pololu motor drivers

--- chess_move.py
+++ chess_move.py
@@ -101,7 +101,7 @@ for x in range(steps):
 
 chess = ChessMove()
 for i in range(30):
-    chess.move_stepper2(55000, "positive")
-    chess.move_stepper2(55000, "negative")
+    chess.move_stepper2(48000, "positive")
+    chess.move_stepper2(48000, "negative")
 
 GPIO.cleanup()
