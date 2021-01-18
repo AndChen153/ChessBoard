b0ee4c6d1b77d45fdcd8b203a79d94a4566018da
andrew <achen1192@gmail.com>
2021-01-18 13:14:07 -0600

    new

---
+++ motortest.py
@@ -0,0 +1,29 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+DIR = 21 # Directional GPIO Pin
+STEP = 20 # Step GPIO Pin
+CW = 1 # CLockwise Rotation
+CCW = 2 # Counter Clockwise Rotation
+SPR = 200  # Steps per Rotation (360/1.8)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR,CW)
+
+step_count = SPR
+delay = 0.005
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.ouput(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(0.5)
+GPIO.output(DIR,CCW)
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.ouput(STEP, GPIO.LOW)
+    sleep(delay)
