9135fc17b410e6c7bd905d71419c0169438f602e
andrew <achen1192@gmail.com>
2021-01-20 14:22:25 -0600

    pieces

--- chess_move.py
+++ chess_move.py
@@ -78,10 +78,10 @@ class ChessMove:
             sleep(self.delay)
         
     def move_steppers_uneven(self, xSquares, ySquares, xdirection, ydirection, mag, knight):
-        GPIO.output(self.POWER, GPIO.HIGH)
         GPIO.output(self.DIR1, self.direction_xdict[xdirection])
         GPIO.output(self.DIR2, self.direction_ydict[ydirection])
         GPIO.output(self.MAGNET, self.magnet_dict[mag])
+        GPIO.output(self.POWER, GPIO.HIGH)
 
         if xSquares < ySquares:
             squares = xSquares
