2d7d453b201cb21aa3cf08d71d814b82bbcb7188
andrew <achen1192@gmail.com>
2021-01-20 21:55:06 -0600

    take piece

--- chess_move.py
+++ chess_move.py
@@ -149,29 +149,26 @@ class ChessMove:
         
         self.move_steppers(squareSteps)
         
-        '''if move_position[0] == 7:
-            GPIO.output(self.DIR1, self.direction_xdict["negative"])    # set stepper direction
-            return_dir = "positive"
-        else:
-            GPIO.output(self.DIR1, self.direction_xdict["positive"])    # set stepper direction
-            return_dir = "negative"
+        GPIO.output(self.DIR1, self.direction_xdict["positive"])    # set stepper direction
         GPIO.output(self.DIR2, self.direction_ydict["negative"])
-        GPIO.output(self.MAGNET, self.magnet_dict["on"])             # set electromagnet position
+        GPIO.output(self.MAGNET, self.magnet_dict["on"])            # set electromagnet position
 
         steps = move_position[1]*self.SPS+self.SPR
         self.move_stepper1(self.HALFSPS)
         self.move_stepper2(steps)
-        GPIO.output(self.MAGNET, GPIO.LOW)
+        GPIO.output(self.MAGNET, self.magnet_dict["off"])
 
 
-        GPIO.output(self.DIR1, self.direction_xdict[return_dir])  
+        GPIO.output(self.DIR1, self.direction_xdict["negative"])  
         GPIO.output(self.DIR2, self.direction_ydict["positive"])
+
         if xfirst:
             self.move_stepper1(remainSteps+self.HALFSPS)
+            self.move_stepper2(self.SPR)
         else:
             self.move_stepper2(steps-remainSteps-self.SPR)
         
-        self.move_steppers(squareSteps)'''
+        self.move_steppers(squareSteps)
 
         sleep(0.5)
         GPIO.output(self.POWER, GPIO.LOW)
